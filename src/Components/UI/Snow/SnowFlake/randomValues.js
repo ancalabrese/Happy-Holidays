e7e51f6a0e00ba3e933cc65c9e8264e4b8b1db1d
Antonio <antoniobust@gmail.com>
2020-12-31 21:14:17 +0100

    Minor graphic adjustments

--- src/Components/UI/Snow/SnowFlake/randomValues.js
+++ src/Components/UI/Snow/SnowFlake/randomValues.js
@@ -1,3 +1,3 @@
 export const hOffset = () => { return Math.floor(Math.random() * (100 - 0)) + '%'; }
 export const vOffset = () => { return '-' + (Math.random() * (50 - 10) + 10) + '%'; }
-export const fallDelay =() => {return Math.floor(Math.random() * (10 - 0)) + 's';}
+export const fallDelay =() => {return Math.floor(Math.random() * (12 - 0)) + 's';}
