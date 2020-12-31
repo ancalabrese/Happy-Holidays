8b132718ca9e3fd495a4d0238ebdfda2acab38fb
Antonio <antoniobust@gmail.com>
2020-12-31 12:44:48 +0100

    Fixed typos

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,12 +7,22 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Bubble from '../UI/Bubble/Bubble';
+import ReactGa from 'react-ga';
 
 function App() {
   const bubbles = Array(130);
   for (var i = 0; i < bubbles.length; i++) {
     bubbles[i] = <Bubble key={i} />
   }
+
+  const onLinkClick = (link) => {
+    ReactGa.event({
+        category: "Website reach out",
+        action: "Clicked on contact item: " + link,
+    });
+
+    window.open(link, '_blank', 'noopener', 'me', 'noreferrer');
+}
 
   return (
     <Container fluid>
@@ -24,13 +34,10 @@ function App() {
         <Col className={"align-items-center"} xs={{ span: 12 }} md={{ span: 6 }} lg={{ span: 4 }} >
           <RoundWishBox />
         </Col>
-        <Row>
-          <Col className={"align-items-end"}>
-            <p>Created By:<a href="https://antonio-calabarese.com/">Antonio Calabrese</a></p>
-          </Col>
-        </Row>
-
       </Row>
+      <div className={Style.credits} onClick={() => onLinkClick("https://antonio-calabrese.com")}>
+        Created By: Antonio Calabrese.
+      </div>
     </Container>
   );
 }
